b39fa9ca6c43879259e6e53624026a0b827a3df2
freewind <nowindlee@gmail.com>
2018-08-09 15:16:36 +0800

    demo

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Kotlin Ktor Demo
 
 Use [ktor](http://ktor.io) to write web server.
 
-Run `Hello.kt` in your IDE.
+Run `Hello.kt` in your IDE, and open <http://localhost:8080> in your browser.
+
+需要注意的坑
+------
+
+- 就算没有对gson进行任何配置，`gson {}`也需要写上，否则访问相关的url返回的是404，没有任何提示。提了一个Issue: <https://github.com/ktorio/ktor/issues/512>
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,8 +1,10 @@
 package example
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
@@ -10,11 +12,17 @@ import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            gson {}
+        }
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                val hello = Hello("Hello Ktor", System.currentTimeMillis())
+                call.respond(hello)
             }
         }
     }
     server.start(wait = true)
 }
+
+data class Hello(val content: String, val timestamp: Long)
